c92d2bf8d6b9e942d4fa200c3f6da4e2409c1ea5
mohammad <40648884+mohammadZx@users.noreply.github.com>
2021-01-13 22:31:21 +0330

    change just

---
+++ app/Providers/PaginationProviders.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Providers;
+// service provider
+use Illuminate\Pagination\Paginator;
+class PaginationProviders{
+    
+    public function register(){
+        Paginator::currentPathResolver(function () {
+            return request()->url();
+        });
+        Paginator::currentPageResolver(function ($pageName = 'page') {
+            $page = request()->$pageName;
+
+            if (filter_var($page, FILTER_VALIDATE_INT) !== false && (int) $page >= 1) {
+                return (int) $page;
+            }
+            return 1;
+        });
+    }
+}
