65ebb39be6977367379361772f1c7a9893070b85
mohammad <40648884+mohammadZx@users.noreply.github.com>
2021-01-13 22:52:31 +0330

    todev

---
+++ bootstrap/cache/9dbb91a15eb43e5db9da9e22ce61d3cade2b9ee6.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+hello
+
+<?php echo e($test); ?>
+
+<br>
+</body>
+</html><?php /**PATH C:\xampp\htdocs\gitrepo\mvcecomlog\resources\view/test.blade.php ENDPATH**/ ?>
--- config/app.php
+++ config/app.php
@@ -3,6 +3,9 @@ $providers = [
     App\Providers\PaginationProviders::class
 ];
 
+
+// create object from provider
+
 foreach ($providers as $provider){
     $pv = new $provider;
     $pv->register();
